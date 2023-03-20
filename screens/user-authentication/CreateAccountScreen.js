3b1fcb9d61af6f80e06b4f006d0475d853a2735b
Kovid63 <kovid63@gmail.com>
2023-03-20 18:01:31 +0530

    createAccountScreen done

--- screens/user-authentication/CreateAccountScreen.js
+++ screens/user-authentication/CreateAccountScreen.js
@@ -1,12 +1,77 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet } from 'react-native'
 import { colors } from '../../colors'
+import FormInput from '../../components/FormInput'
+import CheckBox from '../../components/CheckBox'
+import GetStartedBtn from '../../components/GetStartedBtn'
 
-const CreateAccountScreen = () => {
+const CreateAccountScreen = ({navigation}) => {
+
+    const [company, setCompany] = useState('');
+    const [companyError, setCompanyError] = useState(false);
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState(false);
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState(false);
+    const [isCheckboxTicked, setIsCheckBoxTicked] = useState(false);
+
+    const isButtonActive = !(emailError || passwordError || companyError) && !(email.length == 0 || password.length == 0 || company.length == 0) && isCheckboxTicked;
+
+    function getCompany(company) {
+        setCompany(company);
+    }
+
+    function getEmail(email) {
+        setEmail(email);
+    }
+
+    function getPassword(password) {
+        setPassword(password);
+    }
+
+    function getCompanyError(error) {
+        setCompanyError(error);
+    }
+
+    function getEmailError(error) {
+        setEmailError(error);
+    }
+
+    function getPasswordError(error) {
+        setPasswordError(error);
+    }
+
+    function loginAccount() {
+        navigation.goBack();
+    }
+
+    function getCheckboxStatus(status){
+        setIsCheckBoxTicked(status);
+    }
+
+
+
     return (
         <View style={styles.container}>
-
+            <View style={styles.create}>
+                <Text style={styles.createText}>{'Create New Account'}</Text>
+            </View>
+            <View style={styles.form}>
+                <FormInput placeholder={'Company Name'} getValue={getCompany} getError={getCompanyError}/>
+                <FormInput placeholder={'Email'} getValue={getEmail} getError={getEmailError}/>
+                <FormInput secure={true} placeholder={'Password'} getValue={getPassword} getError={getPasswordError}/>
+            </View>
+            <View style={styles.boxBtnContainer}>
+                <CheckBox isActive={getCheckboxStatus}/>
+                <Text style={styles.agreeText}>{'I Agree to the'}<Text style={{color: colors.primary[0]}}>{' '}{'Terms of Service'}</Text><Text>{' '}{'and'}</Text><Text style={{color: colors.primary[0]}}>{' '}{'Privacy Policy'}</Text></Text>
+            </View>
+            <View style={styles.loginAccContainer}>
+                <Text style={styles.accRequestText}>{"Have an account?"}{' '}<Text onPress={loginAccount} style={styles.loginAccText}>{'Login'}</Text></Text>
+            </View>
+            <View style={styles.submitBtnContainer}>
+                <GetStartedBtn active={isButtonActive}/>
+            </View>
         </View>
     )
 }
@@ -19,6 +84,52 @@ const styles = StyleSheet.create({
         paddingHorizontal: '5%',
         justifyContent: 'space-evenly'
     },
+
+    create: {
+        marginTop: '15%'
+    },
+
+    createText: {
+        fontFamily: 'PoppinsSemiBold',
+        fontSize: 42,
+        color: colors.black[0]
+    },
+
+    form: {
+        justifyContent: 'space-between'
+    },
+
+    boxBtnContainer: {
+        marginHorizontal: '3%',
+        flexDirection: 'row',
+        width: '70%'
+    },
+
+    agreeText: {
+        marginLeft: '3%',
+        fontFamily: 'Poppins',
+        color: colors.black[4],
+        fontSize: 12
+    },
+
+    loginAccContainer: {
+        alignItems: 'center',
+        marginTop: '25%'
+    },
+
+    accRequestText: {
+        fontFamily: 'Poppins',
+        fontSize: 13
+    },
+
+    loginAccText: {
+        fontFamily: 'PoppinsSemiBold',
+        color: colors.primary[0]
+    },
+
+    submitBtnContainer: {
+        alignItems: 'center'
+    }
 })
 
 export default CreateAccountScreen
