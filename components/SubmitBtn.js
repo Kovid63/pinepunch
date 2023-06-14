08ddeff160537e68dc355b3629370d3e4a26d1a3
Kovid <kovid63@gmail.com>
2023-06-14 10:03:12 +0530

    white loading gif added

--- components/SubmitBtn.js
+++ components/SubmitBtn.js
@@ -6,7 +6,7 @@ import { colors } from '../colors'
 const SubmitBtn = ({ active, text, onPress, outline, fill, isLoading }) => {
   return (
     <TouchableOpacity onPress={onPress} disabled={active && !isLoading? false : true} activeOpacity={active ? 0.5 : 1} style={[styles.container, fill ? { backgroundColor: colors.primary[0] } : outline? { borderColor: colors.primary[0], borderWidth: 1 } : { backgroundColor: colors.black[5] }]}>
-      { isLoading? <Image source={require('../assets/loader.gif')} style={{height: 50, width: 100}} /> : <Text style={[styles.btnText, outline? {color: colors.primary[0]}: {}]}>{text}</Text>}
+      { isLoading? <Image source={require('../assets/LoadingWhite.gif')} style={{height: 50, width: 100}} /> : <Text style={[styles.btnText, outline? {color: colors.primary[0]}: {}]}>{text}</Text>}
     </TouchableOpacity>
   )
 }
